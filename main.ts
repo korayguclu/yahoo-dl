d593f9bb1c7eb9b50fcead6fc583cbed3cac8cf9
Koray Guclu <korayguclu@gmail.com>
2018-10-21 23:12:24 +0200

    get url

--- main.ts
+++ main.ts
@@ -1,26 +1,34 @@
 import commander from 'commander';
 import getCookieBySendingADummyRequest from './src/getCookie';
+import getUrl from './src/getUrl';
 import parseCookie from './src/parseCookie';
 import ora from 'ora'
 
 let cli = new commander.Command();
 cli.name('yahoo-dl')
 .version('1.0.0')
-.option('-t, --timeframe <d,w,m>','Timeframe d for daily, w for weekly, m for monthly.') 
+.option('-t, --timeframe <d,w,m>','Timeframe d for daily, w for weekly, m for monthly.','w') 
+.option('-p, --period','Period of the selected time frame e.g. 200, 12 etc...', 104 ) 
 .option('-o, --output <output_path>','Output path.') 
 .description('Yahoo stock price downloader.');
 
+
 cli.command('get <symbol> ')
-    .action((symbol)=>{
-        const spinner = new ora(`Getting symbol ${symbol}`).start();
-        getCookieBySendingADummyRequest(symbol).then((response)=>{
+    .action((symbol,options)=>{
+        const { timeframe, period } = cli.opts().timeframe;
+        const spinner =  ora();
+        spinner.start(`Getting symbol ${symbol} for timeframe ${timeframe}`);
+        const cookies = getCookieBySendingADummyRequest(symbol).then((response)=>{
             const cookies = parseCookie(response);
-            console.log(cookies);
             spinner.succeed();
+            return cookies;
         }).catch((err)=>{
-            spinner.fail(); 
+            spinner.fail();
+            return err;
         });
-        spinner.stop();
+        const url = getUrl(timeframe,period,symbol, cookies['crumb']);
+        console.log("URL:",url);
+
     });
 
 cli.parse(process.argv);
---
+++ src/getUrl.ts
@@ -0,0 +1,14 @@
+import * as util from 'util';
+import { yahooDownload,timeframes } from './constants';
+import moment from 'moment';
+
+const getUrl = (selectedTimeframe,selectedPeriod,symbol,crumb) => {
+    const currentDate = moment();
+    const end = moment(currentDate);
+    const start = moment(currentDate).subtract( selectedPeriod,selectedTimeframe) ;
+    const interval = timeframes[selectedTimeframe] ;
+    const downloadUrl = util.format( yahooDownload,symbol,start.unix(),end.unix(),interval,crumb );
+    return downloadUrl;
+};
+
+export default getUrl;
